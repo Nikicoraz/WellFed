3ecdfc17004a629e525a75b1c92b2dc1f6437537
Nikicoraz <nicolacorato05@gmail.com>
2025-11-07 13:57:14 +0100

    regole extra

--- eslint.config.ts
+++ eslint.config.ts
@@ -14,13 +14,16 @@ export default defineConfig([
       'no-duplicate-imports': 'error',
       'arrow-body-style': ['warn', 'always'],
       'camelcase': 'warn',
-      'default-case': 'error', // https://eslint.org/docs/latest/rules/default-casem
+      'default-case': 'error', // https://eslint.org/docs/latest/rules/default-case
       'default-param-last': 'error', // https://eslint.org/docs/latest/rules/default-param-last
       'prefer-const': 'warn',
       'semi': ['error', 'always'],
       'brace-style': ['warn', '1tbs'],
       'indent': ['warn', 4],
       'space-before-blocks': ['warn', 'always'],
+      'sort-imports': 'warn',
+      'require-await': 'error',
+      'no-undefined': 'error',
     }
 },
   tseslint.configs.recommended,
