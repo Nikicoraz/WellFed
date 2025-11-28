7c51bc18de95fbb1b1039c5892a040a0b85a3804
Nikicoraz <nicolacorato05@gmail.com>
2025-11-28 13:49:36 +0100

    setup database e esempio di modello

---
+++ src/models/product.ts
@@ -0,0 +1,9 @@
+import mongoose, { Schema } from "mongoose";
+
+export default mongoose.model("Product", new Schema({
+    name: String,
+    description: String,
+    origin: String,
+    image: String,
+    points: Number
+}), "Products");
