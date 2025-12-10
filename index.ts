d2ebb02da75092d661c08a698882f0afec8625d6
Nikicoraz <nicolacorato05@gmail.com>
2025-12-10 09:05:01 +0100

    Supporto per la stringa di connessione di atlas

--- index.ts
+++ index.ts
@@ -4,9 +4,10 @@ import mongoose from 'mongoose';
 
 
 const PORT = process.env.PORT || 3000;
-const connectOptions: mongoose.ConnectOptions = process.env.ADMIN == "true" ? {authSource: "admin", dbName: "WellFed"} : { dbName: "WellFed"};
+const connectOptions: mongoose.ConnectOptions = process.env.ADMIN == "true" ? { authSource: "admin", dbName: "WellFed" } : { dbName: "WellFed" };
+const connectionString = process.env.ATLAS_CONNECTION == "true" ? `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}` : `mongodb://${process.env.DB_USER!}:${process.env.DB_PASSWORD!}@${process.env.DB_URL!}:27017`;
 
-app.locals.db = mongoose.connect(`mongodb://${process.env.DB_USER!}:${process.env.DB_PASSWORD!}@${process.env.DB_URL!}:27017`, connectOptions).then(() => {
+app.locals.db = mongoose.connect(connectionString, connectOptions).then(() => {
     app.listen(PORT, () => {
         console.log(`Server listening on http://localhost:${PORT}`);
     });
