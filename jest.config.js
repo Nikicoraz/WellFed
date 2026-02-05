95a62d5b2d6dab0f1d8c5413da9dd1f462ef814e
ManMls <mollismanuel07@gmail.com>
2026-02-05 12:19:25 +0100

    Aggiunta suite 10 + fix jest codecoverage old files in cache and models

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,9 @@ export default {
     ],
     verbose: true,
     collectCoverage: true,
+    collectCoverageFrom: [
+        "<rootDir>/dist/src/**/*.js",
+        "!<rootDir>/dist/src/models/*",
+    ],
     setupFilesAfterEnv: ["./dist/src/__test__/jest.setup.js"]
 };
