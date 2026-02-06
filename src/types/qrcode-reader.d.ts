bc9c18c77d3b0fd65e557372539be254bcf4a879
Nikicoraz <nicolacorato05@gmail.com>
2026-02-06 19:06:35 +0100

    Rimossa cartella non più utilizzata e riorganizzato package.json

--- src/types/qrcode-reader.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "qrcode-reader" {    // Serve a src/__test__/06_qrcode-scan.test.ts per l'import del modulo
-  class QrCode {
-      callback: (err: Error | null, value: { result: string }) => void;
-      decode(bitmap: unknown): void;
-  }
-  export = QrCode;
-}
